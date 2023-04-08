038eb2696327010f4deca0933b18c5ec96f6d386
Code-byme <aminefadili.cg@gmail.com>
2023-04-08 11:42:36 +0000

    fixed

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ user, children }) => {
     if (!user) {
-      return <Navigate to="/auth/login" replace />;
+      return <Navigate to="/" replace />;
     }
   
     return children;
